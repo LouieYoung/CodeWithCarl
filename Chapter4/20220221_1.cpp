f659bc9c7eb9d860213147e05a29ef8b31a3a0b3
如意羊 <438688270@qq.com>
2022-02-23 09:57:51 +0800

    Update 20220221_1.cpp

--- Chapter4/20220221_1.cpp
+++ Chapter4/20220221_1.cpp
@@ -104,36 +104,3 @@ public:
  * obj->addAtIndex(index,val);
  * obj->deleteAtIndex(index);
  */
-
-    }
-    
-    void addAtHead(int val) {
-        
-
-    }
-    
-    void addAtTail(int val) {
-
-    }
-    
-    void addAtIndex(int index, int val) {
-
-    }
-    
-    void deleteAtIndex(int index) {
-
-    }
-    private:
-    int _size;
-    LinkedNode* _dummyHead;
-};
-
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList* obj = new MyLinkedList();
- * int param_1 = obj->get(index);
- * obj->addAtHead(val);
- * obj->addAtTail(val);
- * obj->addAtIndex(index,val);
- * obj->deleteAtIndex(index);
- */
